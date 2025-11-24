b6499d8b27f6222a7e9310fd8a0e960809988e54
ayshabdulla <ayshfanay@gmail.com>
2025-11-24 20:14:19 +0530

    end1

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -6,7 +6,7 @@ const router = require('./Routes/userRoutes');
 const { connection, connect } = require('mongoose');
 
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json())
 app.use(cors())
