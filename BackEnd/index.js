ea7c9484d9ee31bce12772da3a09bec475ea6139
ayshabdulla <ayshfanay@gmail.com>
2025-11-24 21:57:42 +0530

    last commit

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -9,7 +9,12 @@ const app = express()
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json())
-app.use(cors())
+
+app.use(cors({
+  origin: "https://verification-frontend-zqpd.onrender.com",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 
 //Routes
